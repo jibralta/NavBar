5d5528f5c4f539f6cbeac1c3cf9cc35aebc060c4
HappyHappyJoyJoy <joyumali@gmail.com>
2016-07-22 14:13:01 -0700

    Removed CoreData. NSException still.

--- Navigation Bar/ViewController.swift
+++ Navigation Bar/ViewController.swift
@@ -13,7 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        func result() {
+            // what is going to happen every second
+            print("1 second has passed.")
+        }
+        
+        
+        var timer = NSTimer()   // NS = NextStep
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("result"), userInfo: nil, repeats: true)    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
